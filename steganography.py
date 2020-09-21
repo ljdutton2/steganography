a9a2cb61c2d52120785a127fd021cc01ee26de81
lauren <ljdutton2@icloud.com>
2020-09-21 17:17:05 -0500

    decode

--- steganography.py
+++ steganography.py
@@ -1,21 +1,8 @@
-"""
-[Day 7] Assignment: Steganography
-    - Turn in on Gradescope (https://make.sc/bew2.3-gradescope)
-    - Lesson Plan: https://make-school-courses.github.io/BEW-2.3-Web-Security/#/Lessons/Steganography
-
-Deliverables:
-    1. All TODOs in this file.
-    2. Decoded sample image with secret text revealed
-    3. Your own image encoded with hidden secret text!
-"""
-# TODO: Run `pip3 install Pillow` before running the code.
 from PIL import Image
 
 
 def decode_image(path_to_png):
-    """
-    TODO: Add docstring and complete implementation.
-    """
+    
     # Open the image using PIL:
     encoded_image = Image.open(path_to_png)
 
@@ -24,11 +11,25 @@ def decode_image(path_to_png):
 
     # Create a new PIL image with the same size as the encoded image:
     decoded_image = Image.new("RGB", encoded_image.size)
-    pixels = decoded_image.load()
+    #pixels = decoded_image.load()
     x_size, y_size = encoded_image.size
 
-    # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
-    print(red_channel)  # Start coding here!
+    for x in range(x_size):
+        for y in range(y_size):
+            # convert each red channel pixel value to binary string
+            binary = bin(red_channel.getpixel((x,y)))
+            # check if string ends in 0 or 1
+            count = len(binary)
+            lsb = binary[count-1]
+            # ends in 0, pixel at x_size, y_size = black
+            if lsb == "0":
+                decoded_image.putpixel((x,y), (0,0,0))
+            # ends in 1, pixels at x_size, y_size = white
+            else:
+                decoded_image.putpixel((x,y), (255,255,255))
+    
+    # DO NOT MODIFY. Save the decoded image to disk:
+    decoded_image.save("decoded_image.png")
 
     # DO NOT MODIFY. Save the decoded image to disk:
     decoded_image.save("decoded_image.png")
@@ -39,3 +40,12 @@ def encode_image(path_to_png):
     TODO: Add docstring and complete implementation.
     """
     pass
+
+def write_text(text_to_write):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass
+
+if __name__ == "__main__":
+    decode_image('encoded_image.png')
